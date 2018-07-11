21b62546d28ee5c6fd697bdfdf48ca43652c4d18
Brandon T <JustBrandonT@gmail.com>
2018-07-10 20:37:54 -0400

    Fixed Issue where devices don't show up.

--- iOSNXLauncher/iOSNXLauncher/NXLauncher.mm
+++ iOSNXLauncher/iOSNXLauncher/NXLauncher.mm
@@ -17,6 +17,20 @@
 #include "NXUSBDevice.hpp"
 #include "fusee.h"
 
+
+extern "C" {
+    #import <IOKit/IOKitLib.h>
+    #import <IOKit/usb/IOUSBLib.h>
+    #import <IOKit/IOCFPlugIn.h>
+    #import <IOKit/IOMessage.h>
+    #import <IOKit/IOBSD.h>
+    #import <CoreFoundation/CoreFoundation.h>
+}
+
+#include <iostream>
+#include <vector>
+#include <array>
+
 std::vector<std::uint8_t> loadIntermezzo()
 {
     const std::string intermezzo_path = [[NSBundle mainBundle] pathForResource:@"intermezzo" ofType:@"bin"].UTF8String ?: "";
@@ -93,6 +107,8 @@ std::vector<std::uint8_t> createPayload(std::vector<std::uint8_t> intermezzo, st
 
 
 #ifdef USB_LIB_USB
+void DeviceAdded(void *userInfo, io_iterator_t iterator);
+
 @implementation Smash
 + (void)smash {
     USBDeviceManager manager = USBDeviceManager();
@@ -128,42 +144,280 @@ std::vector<std::uint8_t> createPayload(std::vector<std::uint8_t> intermezzo, st
     
     if (!devices.size())
     {
-        [ViewController.logger appendString:@"No Devices Connected"];
+        CFMutableDictionaryRef matchingDict;
+        io_iterator_t iter;
+        kern_return_t kr;
+        io_service_t device;
         
-        libusb_context *context = NULL;
-        libusb_device **list = NULL;
+        const std::uint16_t kNintendoSwitchVendorID = 0x0955;
+        const std::uint16_t kNintendoSwitchProductID = 0x7321;
+        NSMutableArray *strings = [[NSMutableArray alloc] init];
+        
+        matchingDict = IOServiceMatching("IOUSBHostDevice"); //kIOUSBDeviceClassName for OSX but "IOUSBHostDevice" for iOS..
+        if (matchingDict == nil)
+        {
+            return;
+        }
+        
+        //Add Nintendo Switch
+        long usbVendor = kNintendoSwitchVendorID;
+        long usbProduct = kNintendoSwitchProductID;
+        CFNumberRef numberRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type, &usbVendor);
+        CFDictionarySetValue(matchingDict, CFSTR(kUSBVendorID), numberRef);
+        CFRelease(numberRef);
 
-        int rc = libusb_init(&context);
-        if (rc != 0)
+        // Create a CFNumber for the idProduct and set the value in the dictionary
+        numberRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type, &usbProduct);
+        CFDictionarySetValue(matchingDict, CFSTR(kUSBProductID), numberRef);
+        CFRelease(numberRef);
+        numberRef = nil;
+        
+        kr = IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, &iter);
+        if (kr != KERN_SUCCESS)
         {
-            [ViewController.logger appendString:@"Error Initializing LibUSB"];
             return;
         }
         
-        ssize_t count = libusb_get_device_list(context, &list);
-        if (count <= 0)
+        // Iterate over all usb devices..
+        while ((device = IOIteratorNext(iter)))
         {
-            [ViewController.logger appendString:@"Failed to Detect Any Devices.."];
-            return;
+            io_name_t devName;
+            io_name_t className;
+            io_string_t pathName;
+            io_string_t planeName;
+            
+            IORegistryEntryGetName(device, devName);
+            IOObjectGetClass(device, className);
+            IORegistryEntryGetPath(device, kIOServicePlane, pathName);
+            IORegistryEntryGetPath(device, kIOUSBPlane, planeName);
+            
+            int vendorId = [[self class] getDeviceVendorId:device];
+            int productId = [[self class] getDeviceProductId:device];
+            NSString *serialNumber = [[self class] getDeviceSerialNumber:device];
+            NSString *manufacturer = [[self class] getDeviceManufacturer:device];
+            
+            [strings addObject:[NSString stringWithFormat:@"Device Name: %s", devName]];
+            [strings addObject:[NSString stringWithFormat:@"Device Class: %s", className]];
+            [strings addObject:[NSString stringWithFormat:@"Device Plane: %s", pathName]];
+            [strings addObject:[NSString stringWithFormat:@"Device Path: %s", planeName]];
+            
+            [strings addObject:[NSString stringWithFormat:@"VendorID: %04X", vendorId]];
+            [strings addObject:[NSString stringWithFormat:@"ProductID: %04X", productId]];
+            [strings addObject:[NSString stringWithFormat:@"Device Serial Number: %@", serialNumber]];
+            [strings addObject:[NSString stringWithFormat:@"Device Manufacturer: %@", manufacturer]];
+            
+            [strings addObject:@"\n"];
+            DeviceAdded(nullptr, device);
+            IOObjectRelease(device);
+            
+            for (NSString *str : strings) {
+                [ViewController.logger appendString:[NSString stringWithFormat:@"%@\n", str]];
+            }
         }
         
-        [ViewController.logger appendString:@"Detected Devices: \n\n"];
-        
-        for (size_t idx = 0; idx < count; ++idx)
-        {
-            libusb_device *device = list[idx];
-            libusb_device_descriptor desc = {0};
-            
-            rc = libusb_get_device_descriptor(device, &desc);
-            assert(rc == 0);
-            
-            printf("VendorID: %04x\nProductID: %04x\n", desc.idVendor, desc.idProduct);
-        }
-        
-        libusb_free_device_list(list, 1);
-        libusb_exit(context);
+        IOObjectRelease(iter);
     }
 }
+
+void** getInterface(io_service_t device, CFUUIDRef type, CFUUIDRef uuid)
+{
+    // Create a plugin interface for the device.
+    IOCFPlugInInterface **plugInInterface = nil;
+    SInt32 score = 0;
+    
+    kern_return_t kr = IOCreatePlugInInterfaceForService(device, type, kIOCFPlugInInterfaceID, &plugInInterface, &score);
+    
+    if ((kIOReturnSuccess != kr) || !plugInInterface)
+    {
+        return nil;
+    }
+    
+    // Get a USBDeviceInterface from the plugin.. and release the plugin
+    void **deviceInterface = nil;
+    HRESULT res = (*plugInInterface)->QueryInterface(plugInInterface, CFUUIDGetUUIDBytes(uuid), (LPVOID*) &deviceInterface);
+    (*plugInInterface)->Release(plugInInterface);
+    
+    if (res || !deviceInterface)
+    {
+        return nil;
+    }
+    return deviceInterface;
+};
+
+void DeviceAdded(void *userInfo, io_iterator_t iterator)
+{
+    io_iterator_t usbDevice = iterator;
+    
+    //Function to retrieve a USBDevice interface..
+    IOUSBDeviceInterface300** (^getDeviceInterface)(io_service_t) = ^IOUSBDeviceInterface300**(io_service_t device) {
+        return (IOUSBDeviceInterface300 **)getInterface(device, kIOUSBDeviceUserClientTypeID, kIOUSBDeviceInterfaceID300);
+    };
+    
+    //Function to retrieve a USBInterface interface..
+    IOUSBInterfaceInterface300** (^getUSBInterface)(io_service_t) = ^IOUSBInterfaceInterface300**(io_service_t device) {
+        return (IOUSBInterfaceInterface300 **)getInterface(device, kIOUSBInterfaceUserClientTypeID, kIOUSBInterfaceInterfaceID300);
+    };
+    // Open the USB device for communication.
+    IOUSBDeviceInterface300 **deviceInterface = getDeviceInterface(usbDevice);
+    if (deviceInterface)
+    {
+        if ((*deviceInterface)->USBDeviceOpen(deviceInterface) == kIOReturnSuccess)
+        {
+            //Get the configuration..
+            IOUSBConfigurationDescriptorPtr config;
+            kern_return_t kr = (*deviceInterface)->GetConfigurationDescriptorPtr(deviceInterface, 0, &config);
+            if (kr == kIOReturnSuccess)
+            {
+                (*deviceInterface)->SetConfiguration(deviceInterface, config->bConfigurationValue);
+                
+                //Find the USB interface..
+                IOUSBFindInterfaceRequest interfaceRequest;
+                interfaceRequest.bInterfaceClass = kIOUSBFindInterfaceDontCare;
+                interfaceRequest.bInterfaceSubClass = kIOUSBFindInterfaceDontCare;
+                interfaceRequest.bInterfaceProtocol = kIOUSBFindInterfaceDontCare;
+                interfaceRequest.bAlternateSetting = kIOUSBFindInterfaceDontCare;
+                
+                //Get an interface iterator..
+                io_iterator_t iterator;
+                kr = (*deviceInterface)->CreateInterfaceIterator(deviceInterface, &interfaceRequest, &iterator);
+                if (kr == kIOReturnSuccess)
+                {
+                    if ((usbDevice = IOIteratorNext(iterator)))
+                    {
+                        IOUSBInterfaceInterface300 **usbInterface = getUSBInterface(usbDevice);
+                        if (usbInterface)
+                        {
+                            kr = (*usbInterface)->USBInterfaceOpen(usbInterface);
+                            if (kr == kIOReturnSuccess) {
+                                UInt8 pipe_ref = 1;
+                                kr = (*usbInterface)->GetPipeStatus(usbInterface, pipe_ref);
+                                switch (kr) {
+                                    case kIOReturnNoDevice:
+                                        [ViewController.logger appendString:@"Pipe Status: No Device"];
+                                        break;
+                                    case kIOReturnNotOpen:
+                                        [ViewController.logger appendString:@"Pipe Status: Not Open"];
+                                        break;
+                                    case kIOReturnSuccess:
+                                        [ViewController.logger appendString:@"Pipe Status: Open"];
+                                        break;
+                                    case kIOReturnBusy:
+                                        [ViewController.logger appendString:@"Pipe Status: Busy"];
+                                        break;
+                                    default:
+                                        [ViewController.logger appendString:@"Pipe Status: We screwed up"];
+                                        break;
+                                }
+                                
+                                //Testing..
+//                                (*usbInterface)->WritePipe(usbInterface, pipe_ref, data, sizeof(data));
+                            }
+                            
+                            (*usbInterface)->Release(usbInterface);
+                        }
+                        
+                        IOObjectRelease(usbDevice);
+                    }
+                    
+                    IOObjectRelease(iterator);
+                }
+            }
+            
+            (*deviceInterface)->USBDeviceClose(deviceInterface);
+        }
+        
+        (*deviceInterface)->Release(deviceInterface);
+    }
+    
+    // Cleanup
+    IOObjectRelease(usbDevice);
+}
+
++ (int)getUSBProperty:(io_service_t)device propertyName:(NSString *)propertyName {
+    CFNumberRef number = (CFNumberRef)IORegistryEntryCreateCFProperty(device, (__bridge CFStringRef)(propertyName), kCFAllocatorDefault, 0);
+    
+    if (number) {
+        int value = 0;
+        CFNumberGetValue(number, kCFNumberSInt32Type, &value);
+        CFRelease(number);
+        return value;
+    }
+    return -1;
+}
+
++ (int)getDeviceVendorId:(io_service_t)device {
+    return [self getUSBProperty:device propertyName:@"idVendor"];
+}
+
++ (int)getDeviceProductId:(io_service_t)device {
+    return [self getUSBProperty:device propertyName:@"idProduct"];
+}
+
++ (NSString *)getDeviceSerialNumber:(io_service_t)device {
+    IOUSBDeviceInterface300 **deviceInterface = [self getDeviceInterface:device];
+    if (deviceInterface) {
+        UInt8 index;
+        (*deviceInterface)->USBGetSerialNumberStringIndex(deviceInterface, &index);
+        NSString *result = [self getDeviceStringDescriptor:deviceInterface index:index];
+        (*deviceInterface)->Release(deviceInterface);
+        return result;
+    }
+    return nil;
+}
+
++ (NSString *)getDeviceManufacturer:(io_service_t)device {
+    IOUSBDeviceInterface300 **deviceInterface = [self getDeviceInterface:device];
+    if (deviceInterface) {
+        UInt8 index;
+        (*deviceInterface)->USBGetManufacturerStringIndex(deviceInterface, &index);
+        NSString *result = [self getDeviceStringDescriptor:deviceInterface index:index];
+        (*deviceInterface)->Release(deviceInterface);
+        return result;
+    }
+    return nil;
+}
+
++ (IOUSBDeviceInterface300 **)getDeviceInterface:(io_service_t)device {
+    kern_return_t result;
+    SInt32 score;
+    IOCFPlugInInterface **plugin = nil;
+    result = IOCreatePlugInInterfaceForService(device, kIOUSBDeviceUserClientTypeID, kIOCFPlugInInterfaceID, &plugin, &score);
+    if (result != KERN_SUCCESS) {
+        return nil;
+    }
+    
+    IOUSBDeviceInterface300 **deviceInterface = nil;
+    result = (*plugin)->QueryInterface(plugin, CFUUIDGetUUIDBytes(kIOUSBDeviceInterfaceID300), (void **)&deviceInterface);
+    if (result != KERN_SUCCESS) {
+        IODestroyPlugInInterface(plugin);
+        return nil;
+    }
+    IODestroyPlugInInterface(plugin);
+    return deviceInterface;
+}
+
++ (NSString *)getDeviceStringDescriptor:(IOUSBDeviceInterface300 **)deviceInterface index:(uint8_t)index {
+    UInt8 requestBuffer[256];
+    IOUSBDevRequest request = {
+        .bmRequestType = USBmakebmRequestType(kUSBIn, kUSBStandard, kUSBDevice),
+        .bRequest = kUSBRqGetDescriptor,
+        .wValue = static_cast<UInt16>((kUSBStringDesc << 8) | index),
+        .wIndex = 0x409, //English
+        .wLength = sizeof(requestBuffer),
+        .pData = requestBuffer
+    };
+    
+    kern_return_t result;
+    result = (*deviceInterface)->DeviceRequest(deviceInterface, &request);
+    if (result != KERN_SUCCESS) {
+        return nil;
+    }
+    
+    int strLength = requestBuffer[0] - 2;
+    CFStringRef serialNumberString = CFStringCreateWithBytes(kCFAllocatorDefault, &requestBuffer[2], strLength, kCFStringEncodingUTF16LE, false);
+    return (__bridge NSString *)serialNumberString;
+}
+
 @end
 #else
 @implementation Smash
